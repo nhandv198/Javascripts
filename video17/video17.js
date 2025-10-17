b7106e2f77899d8c7f05ea5390a205bef1951335
nhandv198 <nhandv@gmail.com>
2025-10-17 16:59:08 +0700

    20251017

---
+++ video17/video17.js
@@ -0,0 +1,26 @@
+console.log ("video 17");
+const MyName = "NHANDV";
+console.log(MyName);
+
+//Khai bao nhieu bien tren 1 dong
+let a =5, b=10, c=15
+console.log("Total: ",a+b+c)
+
+
+// if 5>3 then 
+let bolobala;
+console.log(5>3)
+console.log(bolobala)
+
+//Object: key-value
+const person = {
+name: "NHANDV",
+age: 15,
+address: "53 Nguyen khoai"
+ }
+ console.log(person);
+
+
+ //Arrary
+ var score= [1,3,5,7,9, 10]
+ console.log(score);
---
+++ video20/operator.js
@@ -0,0 +1,15 @@
+//Khai bao nhieu bien tren 1 dong
+let a=5, b=10,c=15
+total= a+b+c
+console.log ("A=",a,"B=",b,"C=",c)
+console.log ("total a+b+c =",total)
+
+console.log (" a/b =",a/b)
+console.log (" c/a =",c/a)
+console.log (" c%a =",c%a)
+console.log (" a*b =",a*b)
+
+console.log (" a++ =",++a)
+console.log (" b-- =",++b)
+
+
