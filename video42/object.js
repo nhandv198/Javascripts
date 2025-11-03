62d7e62438543ee63a482707a3a734e9e9dfe829
Doan Nhan <nhandv@gmail.com>
2025-11-03 23:28:17 +0700

    20251103

---
+++ video42/object.js
@@ -0,0 +1,20 @@
+let sv1 = {
+    score: 10,
+    name: "NHANDV",
+    address: {
+        city: "hanoi",
+        country: "Vietnam"
+    }
+}
+
+let sv2 = {
+    score: 5,
+    name: "Jack",
+    address: {
+        city: "Paris",
+        country: "France"
+    }
+}
+
+console.log(sv1)
+console.log(sv2)
